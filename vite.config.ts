2db0eb3c661cbd4202f36db71cdffb770734e18d
Andrey Moskovtsev <U369226@danfoss.com>
2023-03-07 11:48:02 +0300

    second commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid';
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+export default defineConfig({
+  plugins: [solidPlugin()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+  },
+});
